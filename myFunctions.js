08920b33fa38b4c80544206217421ebc894aa25b
aliabbas849 <sawim.sy@gmail.com>
2024-06-17 22:55:11 +0300

    Add files via upload

--- myFunctions.js
+++ myFunctions.js
@@ -10,8 +10,7 @@ function toggleDetails(checkbox, detailsId) {
         details.style.display = 'none';
       }
     }
-
-    function goToPurchaseForm() {
+ function goToPurchaseForm() {
       var radios = document.querySelectorAll('input[type="radio"][name="property"]');
       var selected = false;
       for (var i = 0; i < radios.length; i++) {
@@ -30,9 +29,29 @@ function toggleDetails(checkbox, detailsId) {
       }
 return false; // منع تحديث الصفحة
     }
+
 function submitForm() {
   var fullName = document.getElementById('fullName').value;
   var nationalId = document.getElementById('nationalId').value;
+  var mobileNumber = document.getElementById('mobileNumber').value;
+  var email = document.getElementById('email').value;
+  var birthDate = document.getElementById('birthDate').value;
   var selectedProperty = document.querySelector('input[name="property"]:checked').value;
-  alert('الاسم الكامل: ' + fullName + '\nالرقم الوطني: ' + nationalId + '\nالعقار المختار: ' + selectedProperty);
+  
+  // التحقق من صحة رقم الهاتف والرقم الوطني
+  var phonePattern = /^09\d{8}$/; // يجب أن يبدأ بـ "09" ويتبعه 8 أرقام
+  var nationalIdPattern = /^\d{11}$/; // يجب أن يكون 11 رقمًا
+  
+  if (!phonePattern.test(mobileNumber)) {
+    alert('رقم الموبايل يجب أن يكون من 10 أرقام ويبدأ بـ "09"');
+    return false;
+  }
+  
+  if (!nationalIdPattern.test(nationalId)) {
+    alert('الرقم الوطني يجب أن يكون من 11 رقمًا');
+    return false;
+  }
+  
+  alert('تم أستلام طلبك يا: ' + fullName + '\nرقمك الوطني هو: ' + nationalId + '\nتاريخ ميلادك هو: ' + birthDate + '\nبريدك الألكتروني\n:' + email + '\nلقد قمت باختيار العقار: ' + selectedProperty + '\nسيتم الاتصال بك على رقمك التالي خلال 24 ساعة\n' + mobileNumber + '\n ملاحظة: يرجى التأكد من المعلومات المدخلة أعلاه لأنه سيتم التواصل معك عبر أحد وسائل التواصل التي أدخلتها لذلك نرجوا منك التأكد وأن كان هناك خطأ يرجعى أعادة أرسال النوذج مرة أخرى\n الرونق للعقارات.');
+
 }
